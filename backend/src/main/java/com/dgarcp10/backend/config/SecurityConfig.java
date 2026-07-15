8b56928c58e03f3863017337301fc441ede7669c
Diego <dgarcp10@estudiantes.unileon.es>
2026-07-16 00:41:33 +0200

    Feature: import para seguridad en ciertas rutas

--- backend/src/main/java/com/dgarcp10/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/dgarcp10/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.dgarcp10.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
